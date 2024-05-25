74c04f4c464770f3934c017ea1050b240ab04ddc
nicco70 <nicolasgf_98@hotmail.com>
2024-05-25 04:08:50 -0300

    commit final storeApp

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -1,30 +1,45 @@
 import 'react-native-gesture-handler';
 
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './presentation/navigation/StackNavigator';
-import * as eva from '@eva-design/eva';
-import {
-  ApplicationProvider,
-  IconRegistry,
-  Layout,
-  Text,
-} from '@ui-kitten/components';
 import {useColorScheme} from 'react-native';
-import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import {AuthProvider} from './presentation/providers/AuthProvider';
+
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const ProductsApp = () => {
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? eva.dark : eva.light;
-
-  const queryClient = new QueryClient();
+  const backgroundColor =
+    colorScheme === 'dark'
+      ? theme['color-basic-800']
+      : theme['color-basic-100'];
 
   return (
     <QueryClientProvider client={queryClient}>
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={theme}>
-        <NavigationContainer>
-          <StackNavigator />
+        <NavigationContainer
+          theme={{
+            dark: colorScheme === 'dark',
+            colors: {
+              primary: theme['color-primary-500'],
+              background: backgroundColor,
+              card: theme['color-basic-100'],
+              text: theme['text-basic-color'],
+              border: theme['color-basic-800'],
+              notification: theme['color-primary-500'],
+            },
+          }}>
+          <AuthProvider>
+            <StackNavigator />
+          </AuthProvider>
         </NavigationContainer>
       </ApplicationProvider>
     </QueryClientProvider>
